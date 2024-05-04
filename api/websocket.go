17d4206412d629562ecca0ae2308fedcbed55378
netnesanya <Netnesanya@mail.ru>
2024-05-04 21:02:58 +0200

    Accept action:data ws message

--- api/websocket.go
+++ api/websocket.go
@@ -1,11 +1,17 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
+
+type WSMessage struct {
+	Action string `json:"action"`
+	Data   string `json:"data"`
+}
 
 var connections = make(map[string]*websocket.Conn)
 
@@ -18,7 +24,7 @@ func WSHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer RemoveConnection(uid)
-	defer conn.Close()
+	//defer conn.Close()
 
 	connections[uid] = conn
 	fmt.Println("Connected to websocket with token:", uid)
@@ -30,6 +36,25 @@ func WSHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		log.Printf("Received: %s", msg)
+
+		// Parse the incoming message into the WSMessage struct
+		var wsMsg WSMessage
+		err = json.Unmarshal(msg, &wsMsg)
+		if err != nil {
+			log.Println("Error parsing message:", err)
+			continue
+		}
+
+		log.Printf(wsMsg.Action)
+		// Handle the incoming message
+		HandleIncomingMessage(wsMsg, uid)
+	}
+}
+
+func HandleIncomingMessage(msg WSMessage, uid string) {
+	switch msg.Action {
+	case "broadcast":
+		BroadcastMessage(uid, msg.Data)
 	}
 }
 
